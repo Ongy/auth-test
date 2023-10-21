7c5d810718892662b4c284d08a1401fa62dd3314
Markus Ongyerth <ongy@ongy.net>
2023-10-21 06:04:58 +0200

    V1

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	cookieName = "auth"
+	token      = "TOKENVALUE"
+)
+
+func doAuthorize(w http.ResponseWriter, req *http.Request) {
+	var auth string
+	cookies := req.Cookies()
+	for _, cookie := range cookies {
+		fmt.Printf("Cookie: %v\n", cookie)
+		if cookie.Name == cookieName {
+			auth = cookie.Value
+		}
+	}
+
+	fmt.Printf("auth: %q\n", auth)
+	if auth == "" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	if auth != token {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+}
+
+func setToken(w http.ResponseWriter, req *http.Request) {
+	// This should really be done with a fragment in the URL and a bit of JS
+	// But it's easier to script server-side only
+	token := req.URL.Query().Get("token")
+	if token == "" {
+		http.Error(w, "Missing Fragment", http.StatusBadRequest)
+		return
+	}
+
+	redirect := req.URL.Query().Get("target")
+	if redirect == "" {
+		http.Error(w, "Missing target", http.StatusBadRequest)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{Name: cookieName, Value: token})
+	http.Redirect(w, req, redirect, http.StatusFound)
+}
+
+func doRun(ctx context.Context) error {
+	http.DefaultServeMux.HandleFunc("/authorize", doAuthorize)
+	http.DefaultServeMux.HandleFunc("/settoken", setToken)
+
+	server := http.Server{
+		Addr: ":8081",
+	}
+
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		<-ctx.Done()
+
+		server.Close()
+		stop()
+	}()
+
+	fmt.Printf("Going to listen and server\n")
+	if err := server.ListenAndServe(); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	if err := doRun(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "Execution error: %v\n", err)
+		os.Exit(1)
+	}
+}
